f6a7525dd94b0e7aed3be689fcf0a37ce825e30d
Johan Jordaan <johan@inmydiary.com>
2016-10-14 09:31:54 +1100

    Added the linter configs

--- server.js
+++ server.js
@@ -9,43 +9,43 @@ var log = require('./log');
 var config = require('./config');
 
 cloudinary.config({
-  cloud_name: config.cloudinary_cloud,
-  api_key: config.cloudinary_key,
-  api_secret: config.cloudinary_secret,
+	cloud_name: config.cloudinary_cloud,
+	api_key: config.cloudinary_key,
+	api_secret: config.cloudinary_secret,
 });
 
-MongoClient.connect(config.db, function(err, db) {
-	console.log("Connected successfully to server");
+MongoClient.connect(config.db, function (err, db) {
+	console.log('Connected successfully to server');
 	startApp(db);
 });
 
 var startApp = (db) => {
 	var images = db.collection('images');
-	var upload = multer({ dest: 'uploads/' })
+	var upload = multer({ dest: 'uploads/' });
 	var app = express();
-   app.set('port', (process.env.PORT || 3000));
+	app.set('port', (process.env.PORT || 3000));
 	app.use(bodyParser.json());
-	app.post('/upload', upload.single('file'), function(req, res) {
-		console.log(".................",req.body.tags);
-		console.log(".................",req.body.location);
+	app.post('/upload', upload.single('file'), function (req, res) {
+		console.log('.................', req.body.tags);
+		console.log('.................', req.body.location);
 
-		cloudinary.uploader.upload(req.file.path, function(result) {
+		cloudinary.uploader.upload(req.file.path, function (result) {
 			var image = {
 				cloudinary_response: result,
 				location: req.body.location,
 				tags: req.body.tags,
-			}
+			};
 
 			// Save some stuff
-			images.insertOne(image,(result)=>{
+			images.insertOne(image, (result) => {
 				console.log(result);
 			});
 
-			fs.unlink(req.file.path,(err)=>{
-				res.status(200).json({success: true});
-			})
-		},{tags:['love','hate']});
-	})
+			fs.unlink(req.file.path, (err) => {
+				res.status(200).json({ success: true });
+			});
+		}, { tags: ['love', 'hate'] });
+	});
 
 	app.listen(app.get('port'), function () {
 		console.log(`Listening on port ${app.get('port')}!`);
